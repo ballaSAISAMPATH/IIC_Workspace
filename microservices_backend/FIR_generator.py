d927f2faf914f160f83ba7f562ff0b426127dffe
saisampath <saisampath453@gmail.com>
2026-02-17 23:57:29 +0530

    template prompt initiated

--- microservices_backend/FIR_generator.py
+++ microservices_backend/FIR_generator.py
@@ -1,10 +1,32 @@
 from langchain_groq import ChatGroq
 from langgraph.graph import StateGraph, START, END
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 model =  ChatGroq(
-            api_key=os.getenv("GROQ_API_KEY")
+            model= "llama-3.1-8b-instant",
+            api_key=os.getenv("GROQ_API_KEY"),
+            temperature=0.7,
       )
+
+class FirSchema(BaseModel):
+      topic : str = Field(description="The topic on which the description has to be written")
+      des : str =  Field(description="10 line about the topic and 5 questions on the description")
+
+def generate(state:FirSchema):
+      prompt = f"Write a 10 line description on {state.topic} and also write 5 questions on the description"
+      response = model.invoke(prompt)
+      state.des = response.content
+      return state
+
+graph = StateGraph(FirSchema)
+graph.add_node("generate", generate)
+graph.add_edge(START, "generate")
+graph.add_edge("generate", END)
+
+compiled_graph = graph.compile()
+result = compiled_graph.invoke({"topic":"iron man", "des":""})
+print(result["des"])
